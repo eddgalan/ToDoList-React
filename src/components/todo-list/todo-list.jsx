db5b5b8c3d3147fb9b54461a64d3d9b446e0dbee
eddgalan <edd.galan@hotmail.com>
2022-06-02 16:55:11 -0500

    Task storage - Registra la nueva tarea en el arreglo (tasks) - Estilo para las tareas almacenadas

--- src/components/todo-list/todo-list.jsx
+++ src/components/todo-list/todo-list.jsx
@@ -1,13 +1,28 @@
 import React, { useState } from 'react';
 import TaskForm from '../task-form/task-form';
+import Task from '../task/task';
 import './todo-list.css';
 
 function ToDoList() {
+  const [tasks, setTask] = useState([]);
+
+  const addTask = (task) => {
+    if( task.text.trim() ) {
+      task.text = task.text.trim();
+      const tasksUpdated = [task, ...tasks];
+      setTask(tasksUpdated);
+    }
+  }
+
   return (
     <>
-      <TaskForm />
+      <TaskForm onSubmit={ addTask } />
       <div className="todo-list-container">
-        Lista de tareas
+        {
+          tasks.map((task) =>
+            <Task key={ task.id } id={ task.id } text={ task.text } completed={ task.completed } />
+          )
+        }
       </div>
     </>
   );
