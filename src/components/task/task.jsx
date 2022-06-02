65fbd62c4a1c316c5e4ef191b864e58d9af281ec
eddgalan <edd.galan@hotmail.com>
2022-06-02 13:57:26 -0500

    Add Events to task component - Se agregó la declaración de los métodos para completar tareas y eliminar tareas

--- src/components/task/task.jsx
+++ src/components/task/task.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import './task.css';
 import { AiFillDelete } from "react-icons/ai";
 
-function Task({ text, completed }) {
+function Task({ id, text, completed, completeTask, deleteTask }) {
   return(
     <div className={ completed ? 'task-container completed' : 'task-container' }>
-      <div className="task-text">
+      <div className="task-text" onClick={ () => completeTask(id) }>
         { text }
       </div>
-      <div className="task-container-icons">
+      <div className="task-container-icons" onClick={ () => deleteTask(id) }>
         <AiFillDelete className="task-icon" />
       </div>
     </div>
