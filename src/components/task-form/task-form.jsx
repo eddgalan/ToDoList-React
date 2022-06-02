19c2a7fa8044fe8368b281491b792ddcba420ce1
eddgalan <edd.galan@hotmail.com>
2022-06-02 13:58:40 -0500

    Get and send task - Obtiene la tarea el componente task-form y la envía al componente todo-list

--- src/components/task-form/task-form.jsx
+++ src/components/task-form/task-form.jsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './task-form.css';
+import { v4 as uuidv4 } from 'uuid';
 
 function TaskForm(props) {
+  const [input, setInput] = useState('');
+
+  const typeTask = (event) => {
+    setInput(event.target.value);
+  }
+
+  const send = (event) => {
+    event.preventDefault(); // Evita la carga de la página cuando se envía en form
+    const newTask = {
+      id: uuidv4(),
+      text: input,
+      completed: false,
+    }
+    props.onSubmit(newTask);
+  }
+
   return (
-    <form className="task-form">
-      <input type="text" className="task-input" placeholder="Escriba una tarea" name="text"/>
+    <form className="task-form" onSubmit={ send }>
+      <input type="text" className="task-input" placeholder="Escriba una tarea" name="text" onChange={typeTask} />
       <button className="task-button">
         Agregar tarea
       </button>
