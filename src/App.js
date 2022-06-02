3323b12a4340b4066b71bf5f3306016ecc987980
eddgalan <edd.galan@hotmail.com>
2022-06-02 12:21:00 -0500

    Main App - Estructura y estilo principal de la aplicación

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg';
+import logo from './img/AlphaTeamDev-sm.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      App
+      <div className="logo-container">
+        <img className="logo" src={ logo } />
+      </div>
+      <div className="todolist-container">
+        <h1> Mis Tareas </h1>
+      </div>
     </div>
   );
 }
